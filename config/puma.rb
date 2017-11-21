e46ae132ba62b51f9fa247eb2b8c80b36ca2ccc4
carlos <csm89velocista@gmail.com>
2017-11-21 10:24:01 +0100

    upload puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -1,13 +1,10 @@
 require 'puma'
 
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
-threads threads_count, threads_count
 
 preload_app!
 
 rackup      DefaultRackup
-port        ENV['PORT']     || 3000
+port        ENV['PORT']     || 8080
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
