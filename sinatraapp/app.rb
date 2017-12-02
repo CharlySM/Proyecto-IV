2125f07d1276a730e002fbc506747bc6667ea33e
JJ Merelo <jjmerelo@gmail.com>
2017-12-02 10:42:10 +0100

    Pone un puerto de base

--- sinatraapp/app.rb
+++ sinatraapp/app.rb
@@ -6,21 +6,24 @@ require 'sinatra/reloader' if development?
 require 'json'
 
 class Aplicacion < Sinatra::Base
+  
+  set :port, 80
 
-get '/' do
-  content_type :json
-  {:status => 'ok'}.to_json
-end
+  get '/' do
+    content_type :json
+    {:status => 'ok'}.to_json
+  end
 
-get '/equipos' do
-  erb :equipos
-end
+  get '/equipos' do
+    erb :equipos
+  end
+  
+  get '/clasificacion' do
+    erb :clasificacion
+  end
+  
+  get '/jornada' do
+    erb :jornada
+  end
 
-get '/clasificacion' do
-  erb :clasificacion
-end
-
-get '/jornada' do
-  erb :jornada
-end
 end
